fc6c9e329a5ce00211470a02cf668e7948394173
Andrea Borelli <49035646+andreaborelli@users.noreply.github.com>
2022-04-29 15:59:08 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,30 @@
 function Email() {
-  window.open('form.html', "_self")
-};
+  var popup = window.open('form.html', 'email', 'popup');
+  document.getElementById('form').addMessage();
+}
 
 function Submit() {
-  let form = document.getElementById("form").innerHTML = "";
-  Submit();
+  var from = document.getElementById("from").value;
+  var to = document.getElementById("to").value;
+  var object = document.getElementById("object").value;
+  var message = document.getElementById("message").value;
+
+  /*document.getElementById('form').submit();*/
+  /*console.log(sender);
+  console.log(to);
+  console.log(object);*/
+
+document.write(from + " " + to + " " + object + " " + message);
+};
+
+function addMessage() {
+  var addNewMail = document.createElement("div");
+  addNewMail.innerHTML = "messaggio";
+  document.getElementById("submit").appendChild(addNewMail);
 }
+
+
+
+
+/*window.opener.addMessage();*/
+
