593806131a45a3cb04b3199957afd6fd2e471b70
Andrea Borelli <49035646+andreaborelli@users.noreply.github.com>
2022-05-02 17:37:17 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function Email() {
   var popup = window.open('form.html', 'email', 'popup');
   document.getElementById('form').addMessage();
-}
+};
 
 function Submit() {
   var from = document.getElementById("from").value;
@@ -9,20 +9,19 @@ function Submit() {
   var object = document.getElementById("object").value;
   var message = document.getElementById("message").value;
 
-console.log(from + " " + to + " " + object + " " + message);
+//console.log(from + " " + to + " " + object + " " + message);
 window.addMessage(from, to, object, message);
-window.opener.addMessage(to);
+window.opener.addMessage(from, to, object, message);
 window.close();
 };
 
 function addMessage(from, to, object, message) {
   var addNewMail = document.createElement("div");
+ /* addNewMail = document.createElement("input");
+  addNewMail = document.createElement("h3");
+  addNewMail = document.createElement("h4");*/
+  addNewMail.classList.add("emailRow", "emailRow__options", "emailRow__title", "emailRow__message");
   addNewMail.innerHTML = from + " " + to + " " + object + " " + message;
-  document.getElementById("newmessage").appendChild(addNewMail);
-}
-
-
-
-
-/*window.opener.addMessage();*/
+  document.getElementById("newMessage").appendChild(addNewMail);
+};
 
