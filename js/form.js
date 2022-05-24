17434fe3155c1ec175d84c499e7522f365c6382a
Andrea Borelli <49035646+andreaborelli@users.noreply.github.com>
2022-05-24 10:58:32 +0200

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,10 +1,11 @@
 // invio dati al click
 function Submit() {
+  var id = window.opener.generateEmailId();
     var from = document.getElementById("from").value;
     var to = document.getElementById("to").value;
     var object = document.getElementById("object").value;
     var message = document.getElementById("message").value;
-  window.opener.addMessageTosent(from, to, object, message);
+  window.opener.addMessageTosent(id, from, to, object, message);
   //  window.opener.selectFolder('sent');
   window.close();
   }
